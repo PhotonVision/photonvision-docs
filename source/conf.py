1e834f049602d0edac724757ab059d34a76f15da
cttdev <cttewari@gmail.com>
2020-07-12 18:16:57 -0700

    Improving dark mode application performance.

--- source/conf.py
+++ source/conf.py
@@ -63,6 +63,10 @@ html_title = "PhotonVision Docs"
 # html_theme = 'alabaster'
 html_theme = 'sphinx_material'
 
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
