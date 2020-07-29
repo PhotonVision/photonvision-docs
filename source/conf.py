afc910f8a47c49ba08417267de1a7494a9cb813d
Claudius Tewari <cttdev@users.noreply.github.com>
2020-07-28 19:12:45 -0700

    Fix material theme and scaling (#28)

--- source/conf.py
+++ source/conf.py
@@ -59,20 +59,19 @@ html_title = "PhotonVision Docs"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'alabaster'
 html_theme = 'sphinx_material'
 
 html_sidebars = {
     "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
 }
 
+# Enabling the Sphinx Material Theme version dropdown.
+version_dropdown = True
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# html_context  = { 'css_files': [ 'theme_overrides.css' ] }
 
 html_favicon = 'assets/RoundLogo.png'
 html_logo = 'assets/RectLogo.png'
