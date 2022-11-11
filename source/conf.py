6a321946c67a8762629ba080b845eecc9d2fad5e
Colin Wong <cwongmath@gmail.com>
2022-11-10 20:06:15 -0600

    Fix permalink icons (#210)
    * Fix permalink icons
    
    * Update conf.py
    
    * Trigger build

--- source/conf.py
+++ source/conf.py
@@ -70,6 +70,8 @@ html_favicon = 'assets/RoundLogo.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+def setup(app):
+    app.add_css_file('css/pv-icons.css')
 
 pygments_style = "sphinx"
 
